3e40048d9681c1ddde882a45204f3a3cb47977e1
Youssef Saroute <youssef.saroute77@gmail.com>
2024-11-15 15:40:36 +0100

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'open-uri'
+require 'json'
+
+# L'URL de l'API TMDB pour récupérer les films
+url = "https://tmdb.lewagon.com/movie/top_rated"
+
+# Effectuer une requête HTTP pour récupérer les données des films
+response = URI.open(url).read
+movies_data = JSON.parse(response)["results"]
+
+# L'URL de base pour les posters d'images
+base_url = "https://image.tmdb.org/t/p/original"
+
+# Créer les films dans la base de données
+movies_data.each do |movie|
+  url = base_url + movie["poster_path"]
+  Movie.create(
+    title: movie["title"],
+    overview: movie["overview"],
+    poster_url: url,
+    rating: movie["vote_average"]
+  )
+end
+
+puts Movie.last.poster_url
